bbde698aaab1a08817c54ec1495162b1b583f314
Matheus Raposo <matheus_raposo@hotmail.com>
2023-10-07 08:18:27 -0300

    connect wifi and retry if connection drops

--- main.ino
+++ main.ino
@@ -1,17 +1,40 @@
+#include <ESP8266WiFi.h>
+
 int ledPin = 2;
 int inputPin = 5;
 int pirState = LOW;
 int val = 0;
 int count = 0;
 
+void await_connection() {
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
 void setup() {
+    Serial.begin(9600);
+
     pinMode(ledPin, OUTPUT);
     pinMode(inputPin, INPUT);
 
-    Serial.begin(9600);
+    WiFi.begin(ssid, password);
+
+    Serial.print("Connecting");
+    await_connection();
 }
 
 void loop() {
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("Wifi disconnected. Retrying connection..");
+        await_connection();
+    }
+
     val = digitalRead(inputPin);
     if (val == HIGH) {
         digitalWrite(ledPin, HIGH);
